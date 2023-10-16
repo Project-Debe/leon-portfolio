d0fc8ac0472770e5b40381e31a81211d73a1f99f
sam10105 <samkasyoki@gmail.com>
2023-10-16 15:06:29 +0300

    add seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,26 @@ const inter = Inter({
 
 const title = 'Leon Omondi | Independent digital designer';
 const description = 'Independent Kenyan-based digital designer with 6+ years experience.';
+const url = 'https://omondileon.com'
 
 export const metadata: Metadata = {
   title,
   description,
+  // metadataBase: new URL(url),
+  openGraph: {
+    title,
+    description,
+    url,
+    siteName: 'Leon Omondi',
+    type: 'website',
+  },
+  twitter: {
+    title,
+    description,
+    site: '@123manchild',
+    card: 'summary_large_image',
+  },
+  themeColor: '#0c0c0c'
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
